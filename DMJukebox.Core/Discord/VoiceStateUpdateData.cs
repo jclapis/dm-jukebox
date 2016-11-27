948159f91f96bfdb7b1133b9a74c3e9aace60072
Joe Clapis <jclapis@outlook.com>
2016-11-27 14:28:34 -0500

    Started working on voice server connections

---
+++ DMJukebox.Core/Discord/VoiceStateUpdateData.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace DMJukebox.Discord
+{
+    /// <summary>
+    /// This structure is used to notify the server when the
+    /// client joins or leaves a voice channel.
+    /// </summary>
+    [JsonObject]
+    internal class VoiceStateUpdateData
+    {
+        /// <summary>
+        /// The ID of the guild that the channel is in
+        /// </summary>
+        [JsonProperty("guild_id")]
+        public string GuildID { get; set; }
+
+        /// <summary>
+        /// The ID of the voice channel to connect to (set this
+        /// to null in order to disconnect)
+        /// </summary>
+        [JsonProperty("channel_id")]
+        public string ChannelID { get; set; }
+
+        /// <summary>
+        /// True if the client has muted itself, false if it
+        /// can speak
+        /// </summary>
+        [JsonProperty("self_mute")]
+        public bool IsMuted { get; set; }
+
+        /// <summary>
+        /// True if the client has deafened itself (can't
+        /// receive audio), false if it can hear sound
+        /// </summary>
+        [JsonProperty("self_deaf")]
+        public bool IsDeafened { get; set; }
+    }
+}
