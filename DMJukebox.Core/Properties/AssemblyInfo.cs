843db2be16be9da6b08484c2dc1d3122c7354c85
Joe Clapis <jclapis@outlook.com>
2016-12-07 23:45:23 -0500

    Time to start prepping for the official push to the internets, documentation incoming!

--- DMJukebox.Core/Properties/AssemblyInfo.cs
+++ DMJukebox.Core/Properties/AssemblyInfo.cs
@@ -1,30 +1,20 @@
-﻿using System.Resources;
+﻿/* ===================================================
+ * 
+ * This file is part of the DM Jukebox project.
+ * Copyright (c) 2016 Joe Clapis. All Rights Reserved.
+ * 
+ * =================================================== */
+ 
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System.Resources;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("DMJukebox.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyDescription("This is the core backend assembly for DM Jukebox.")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("Joe Clapis")]
 [assembly: AssemblyProduct("DMJukebox.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Joe Clapis. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
