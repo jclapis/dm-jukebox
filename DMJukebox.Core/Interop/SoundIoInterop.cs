49c2a27a9ef332beeda68f92d96aa9e5806a275f
Joe Clapis <jclapis@outlook.com>
2016-12-11 02:18:48 -0500

    Documented SoundIoInterop

--- DMJukebox.Core/Interop/SoundIoInterop.cs
+++ DMJukebox.Core/Interop/SoundIoInterop.cs
@@ -1,13 +1,9 @@
-﻿/* 
- * This file contains C# wrappers for some of the exported functions 
- * defined in soundio.h of the libsoundio project, for interop use.
+﻿/* ===================================================
  * 
- * For more information, please see the documentation at
- * http://libsound.io/doc-1.1.0/soundio_8h.html or the source code at
- * https://github.com/andrewrk/libsoundio.
+ * This file is part of the DM Jukebox project.
+ * Copyright (c) 2016 Joe Clapis. All Rights Reserved.
  * 
- * Copyright (c) 2016 Joe Clapis.
- */
+ * =================================================== */
 
 using System;
 using System.Runtime.InteropServices;
@@ -17,6 +13,11 @@ namespace DMJukebox.Interop
     /// <summary>
     /// This is a utility class that holds the P/Invoke wrappers for libsoundio.
     /// </summary>
+    /// <remarks>
+    /// For more information, please see the documentation at
+    /// http://libsound.io/doc-1.1.0/soundio_8h.html
+    /// or the source code at https://github.com/andrewrk/libsoundio.
+    /// </remarks>
     internal static class SoundIoInterop
     {
         /// <summary>
@@ -354,96 +355,216 @@ namespace DMJukebox.Interop
 
         #region Public API
         
+        /// <summary>
+        /// Initializes a new <see cref="SoundIo"/> context.
+        /// </summary>
+        /// <returns>(<see cref="SoundIo"/>*) The new context</returns>
         public static IntPtr soundio_create()
         {
             return soundio_create_impl();
         }
         
+        /// <summary>
+        /// Frees a <see cref="SoundIo"/> context.
+        /// </summary>
+        /// <param name="soundio">(<see cref="SoundIo"/>*) The context to free</param>
         public static void soundio_destroy(IntPtr soundio)
         {
             soundio_destroy_impl(soundio);
         }
         
+        /// <summary>
+        /// Connects to the first available sound backend for playback.
+        /// </summary>
+        /// <param name="soundio">(<see cref="SoundIo"/>*) The context to connect with</param>
+        /// <returns><see cref="SoundIoError.SoundIoErrorNone"/> if the call worked, or
+        /// an error code if it failed.</returns>
         public static SoundIoError soundio_connect(IntPtr soundio)
         {
             return soundio_connect_impl(soundio);
         }
         
+        /// <summary>
+        /// Updates information for all of the devices connected to the given
+        /// <see cref="SoundIo"/> context.
+        /// </summary>
+        /// <param name="soundio">(<see cref="SoundIo"/>*) The context to refresh</param>
         public static void soundio_flush_events(IntPtr soundio)
         {
             soundio_flush_events_impl(soundio);
         }
-        
+
+        /// <summary>
+        /// Returns the number of output devices available for the given
+        /// <see cref="SoundIo"/> context to use.
+        /// </summary>
+        /// <param name="soundio">(<see cref="SoundIo"/>*) The context to list the devices for</param>
+        /// <returns>The number of available devices the context can use</returns>
         public static int soundio_output_device_count(IntPtr soundio)
         {
             return soundio_output_device_count_impl(soundio);
         }
         
+        /// <summary>
+        /// Returns the <see cref="SoundIoDevice"/> at the given <paramref name="index"/>
+        /// of the given <see cref="SoundIo"/> context.
+        /// </summary>
+        /// <param name="soundio">(<see cref="SoundIo"/>*) The context to use</param>
+        /// <param name="index">The index of the <see cref="SoundIoDevice"/> to get</param>
+        /// <returns>(<see cref="SoundIoDevice"/>*) The device at the given index, or 
+        /// <see cref="IntPtr.Zero"/> if something went wrong (for example, you never called
+        /// <see cref="soundio_flush_events(IntPtr)"/> or passed invalid parameters in.
+        /// </returns>
         public static IntPtr soundio_get_output_device(IntPtr soundio, int index)
         {
             return soundio_get_output_device_impl(soundio, index);
         }
         
+        /// <summary>
+        /// Returns the index of the default output device, or -1 if none are available.
+        /// </summary>
+        /// <param name="soundio">(<see cref="SoundIo"/>*) The context to use</param>
+        /// <returns>The index of the default output device</returns>
         public static int soundio_default_output_device_index(IntPtr soundio)
         {
             return soundio_default_output_device_index_impl(soundio);
         }
 
+        /// <summary>
+        /// Allocates a new <see cref="SoundIoOutStream"/> to play sound back on the
+        /// provided <see cref="SoundIoDevice"/>.
+        /// </summary>
+        /// <param name="device">(<see cref="SoundIoDevice"/>*) The device to play sound with</param>
+        /// <returns>(<see cref="SoundIoOutStream"/>*) The new stream</returns>
         public static IntPtr soundio_outstream_create(IntPtr device)
         {
             return soundio_outstream_create_impl(device);
         }
-        
+
+        /// <summary>
+        /// Opens a <see cref="SoundIoOutStream"/>, preparing it for playback.
+        /// </summary>
+        /// <param name="outstream">(<see cref="SoundIoOutStream"/>*) The stream to open</param>
+        /// <returns><see cref="SoundIoError.SoundIoErrorNone"/> if the call worked, or
+        /// an error code if it failed.</returns>
         public static SoundIoError soundio_outstream_open(IntPtr outstream)
         {
             return soundio_outstream_open_impl(outstream);
         }
-        
+
+        /// <summary>
+        /// Begins audio playback from the <see cref="SoundIoOutStream"/> to the speakers.
+        /// </summary>
+        /// <param name="outstream">(<see cref="SoundIoOutStream"/>*) The stream to start playing</param>
+        /// <returns><see cref="SoundIoError.SoundIoErrorNone"/> if the call worked, or
+        /// an error code if it failed.</returns>
         public static SoundIoError soundio_outstream_start(IntPtr outstream)
         {
             return soundio_outstream_start_impl(outstream);
         }
         
+        /// <summary>
+        /// Frees a <see cref="SoundIoOutStream"/>. Don't call this from the 
+        /// <see cref="SoundIoOutStream.write_callback"/> thread.
+        /// </summary>
+        /// <param name="outstream">(<see cref="SoundIoOutStream"/>*) The stream to free</param>
         public static void soundio_outstream_destroy(IntPtr outstream)
         {
             soundio_outstream_destroy_impl(outstream);
         }
         
+        /// <summary>
+        /// Decrements the reference counter on a <see cref="SoundIoDevice"/>, freeing it
+        /// once the counter reaches zero.
+        /// </summary>
+        /// <param name="device">(<see cref="SoundIoDevice"/>*) The device to unreference</param>
         public static void soundio_device_unref(IntPtr device)
         {
             soundio_device_unref_impl(device);
         }
-        
+
+        /// <summary>
+        /// This should be called within the <see cref="SoundIoOutStream.write_callback"/>
+        /// callback function once you're ready to start writing data out to the speakers.
+        /// </summary>
+        /// <param name="outstream">(<see cref="SoundIoOutStream"/>*) The stream to write the data to</param>
+        /// <param name="areas">(<see cref="SoundIoChannelArea"/>*) An array of buffers to write the 
+        /// outbound audio data into (one per channel)</param>
+        /// <param name="frame_count">The number of audio frames you want to write during this round</param>
+        /// <returns><see cref="SoundIoError.SoundIoErrorNone"/> if the call worked, or
+        /// an error code if it failed.</returns>
         public static SoundIoError soundio_outstream_begin_write(IntPtr outstream, ref IntPtr areas, ref int frame_count)
         {
             return soundio_outstream_begin_write_impl(outstream, ref areas, ref frame_count);
         }
-        
+
+        /// <summary>
+        /// This should be called within the <see cref="SoundIoOutStream.write_callback"/>
+        /// callback function once you're done writing data, which will then be sent to the speakers
+        /// for playback.
+        /// </summary>
+        /// <param name="outstream">(<see cref="SoundIoOutStream"/>*) The stream the data was written to</param>
+        /// <returns><see cref="SoundIoError.SoundIoErrorNone"/> if the call worked, or
+        /// an error code if it failed.</returns>
         public static SoundIoError soundio_outstream_end_write(IntPtr outstream)
         {
             return soundio_outstream_end_write_impl(outstream);
         }
-        
+
+        /// <summary>
+        /// Sets the pause state of audio playback from the given <see cref="SoundIoOutStream"/>.
+        /// Note that libsoundio doesn't have a "stop" function, so this is as close as you can get.
+        /// </summary>
+        /// <param name="outstream">(<see cref="SoundIoOutStream"/>*) The stream to pause/unpause</param>
+        /// <param name="pause">True to pause playback, false to unpause it</param>
+        /// <returns><see cref="SoundIoError.SoundIoErrorNone"/> if the call worked, or
+        /// an error code if it failed.</returns>
         public static SoundIoError soundio_outstream_pause(IntPtr outstream, bool pause)
         {
             return soundio_outstream_pause_impl(outstream, pause);
         }
         
+        /// <summary>
+        /// Returns the preset <see cref="SoundIoChannelLayout"/> for the given layout ID.
+        /// </summary>
+        /// <param name="index">The ID of the <see cref="SoundIoChannelLayout"/> to get</param>
+        /// <returns>(<see cref="SoundIoChannelLayout"/>*) The matching layout</returns>
         public static IntPtr soundio_channel_layout_get_builtin(SoundIoChannelLayoutId index)
         {
             return soundio_channel_layout_get_builtin_impl(index);
         }
         
+        /// <summary>
+        /// Checks whether or not the given <see cref="SoundIoDevice"/> supports the given
+        /// <see cref="SoundIoChannelLayout"/>.
+        /// </summary>
+        /// <param name="device">(<see cref="SoundIoDevice"/>*) The device to check</param>
+        /// <param name="layout">(<see cref="SoundIoChannelLayout"/>*) The layout to check</param>
+        /// <returns>True if the device supports the layout, false if it doesn't.</returns>
         public static bool soundio_device_supports_layout(IntPtr device, IntPtr layout)
         {
             return soundio_device_supports_layout_impl(device, layout);
         }
         
+        /// <summary>
+        /// Gets the index of the given channel within the given <see cref="SoundIoChannelLayout"/>,
+        /// or -1 if the channel isn't contained in that layout.
+        /// </summary>
+        /// <param name="layout">(<see cref="SoundIoChannelLayout"/>*) The layout to find the channel index from</param>
+        /// <param name="channel">The channel to get the index of</param>
+        /// <returns>The index of the channel within the layout, or -1 if it couldn't be found.</returns>
         public static int soundio_channel_layout_find_channel(IntPtr layout, SoundIoChannelId channel)
         {
             return soundio_channel_layout_find_channel_impl(layout, channel);
         }
         
+        /// <summary>
+        /// Returns the default preset <see cref="SoundIoChannelLayout"/> associated with the given
+        /// number of channels.
+        /// </summary>
+        /// <param name="channel_count">The number of channels contained in the output audio</param>
+        /// <returns>(<see cref="SoundIoChannelLayout"/>*) The default layout corresponding to
+        /// the given number of channels</returns>
         public static IntPtr soundio_channel_layout_get_default(int channel_count)
         {
             return soundio_channel_layout_get_default_impl(channel_count);
